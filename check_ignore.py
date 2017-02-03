5167cd6c3938b8e925f13ce971c87c003b58c85e
Gustavo Goretkin <goretkin@csail.mit.edu>
2017-02-03 14:51:33 -0500

    explain why ignore symlinks, verify with git ls-files

--- check_ignore.py
+++ check_ignore.py
@@ -14,5 +14,22 @@ def is_path_git_ignored(git_root, path):
 		return True
 	if o.returncode == 1:
 		return False
-	
+
 	raise RuntimeError("git returned {}. stdout: {} stderr: {}".format(o.returncode, stdout, stderr))
+
+
+def git_ls_files(git_root):
+	"""
+	return a list of paths (bytes-like) returned by git ls-files
+	"""
+	o = subprocess.Popen(["git", "-c", git_root, "ls-files", "-z"],
+		stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	stdout, stderr = o.communicate()
+
+	if o.returncode != 0:
+		raise RuntimeError("git returned {}. stdout: {} stderr: {}".format(o.returncode, stdout, stderr))
+
+	# each path is terminated with zero, so last item should be an empty string
+	paths = stdout.split(b"\x00")
+	assert paths[-1] == b""
+	return paths[:-1]
